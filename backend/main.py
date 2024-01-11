7eb7de6c2d8f567d04060aeacd84e9e309a4da4f
danielel10 <elhadan@post.bgu.ac.il>
2024-01-11 17:51:02 +0200

    Fixed relative root folder

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify , send_file
+from flask import Flask, jsonify, send_file
 from flask_cors import CORS
 from flask import Flask, jsonify, request
 import numpy as np
@@ -9,17 +9,13 @@ import json
 import shutil
 import os
 
-
-
-
 # List of all metallic element symbols
 Metallic_atoms = ['Ac', 'Al', 'Sb', 'Ag', 'Ba', 'Bi', 'Bh', 'B', 'Cd', 'Ca', 'Cf', 'Cr', 'Co', 'Cm',
-                   'Ds', 'Db', 'Dy', 'Fr', 'Gd', 'Ga', 'Au', 'Hf', 'Hs', 'Ho', 'Ir', 'Fe', 'Kr', 'La',
-                     'Li', 'Mc', 'Nh', 'Nb', 'N', 'Os', 'Pd', 'P', 'Pt', 'Pu', 'Po', 'K', 'Pa', 'Ra',
-                       'Rn', 'Re', 'Rh', 'Rb', 'Sm', 'Sc', 'Si', 'Na', 'Sr', 'Ta', 'Tl', 'Th', 'Tm',
-                         'Sn', 'Ti', 'U', 'V', 'Y', 'Zn', 'Zr']
+                  'Ds', 'Db', 'Dy', 'Fr', 'Gd', 'Ga', 'Au', 'Hf', 'Hs', 'Ho', 'Ir', 'Fe', 'Kr', 'La',
+                  'Li', 'Mc', 'Nh', 'Nb', 'N', 'Os', 'Pd', 'P', 'Pt', 'Pu', 'Po', 'K', 'Pa', 'Ra',
+                  'Rn', 'Re', 'Rh', 'Rb', 'Sm', 'Sc', 'Si', 'Na', 'Sr', 'Ta', 'Tl', 'Th', 'Tm',
+                  'Sn', 'Ti', 'U', 'V', 'Y', 'Zn', 'Zr']
 
- 
 # configuration
 DEBUG = True
 
@@ -30,12 +26,9 @@ app.config['FLASK_DEBUG'] = True
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
-
-
-
-
+PLOTS_DIRECTORY = os.path.join(app.root_path, 'plots')
 molecules = [
-    
+
 ]
 
 
@@ -55,34 +48,32 @@ def all_molecules():
             with open(temp_file_path, 'r') as xyz_file:
                 lines = xyz_file.readlines()
                 molecule_name = lines[1].strip()
-                
-            
+
                 elements, coordinates = read_xyz(temp_file_path)
 
                 # this could be the metal atom or another predefined atom by the user
                 try:
-                    center_atom = getcenter(elements,non_metalic)
+                    center_atom = getcenter(elements, non_metalic)
                 except Exception as e:
                     response_object['status'] = 'failure'
                     response_object['message'] = 'no center provided'
                     return jsonify(response_object), 404
-                
-                
+
                 id = uuid.uuid4().hex
-                
+
                 # Create a BuriedVolume object depending on z axios or not
-                bv = BuriedVolume(elements, coordinates, center_atom, excluded_atoms, z_axis_atoms= zaxis_atoms)
-                
+                bv = BuriedVolume(elements, coordinates, center_atom, excluded_atoms, z_axis_atoms=zaxis_atoms)
+
                 # if we want to create a strict map we need zaxis atoms
                 if zaxis_atoms:
                     plot_id = f"plot_{id}.png"
-                    bv.plot_steric_map(filename = plot_id)
-                    shutil.move(plot_id, "backend/plots/")
+                    bv.plot_steric_map(filename=plot_id)
+                    shutil.move(plot_id, PLOTS_DIRECTORY)
                 # Get the fraction of buried volume
                 fraction_buried_volume = bv.fraction_buried_volume
 
                 molecules.append({
-                    'id' : id,
+                    'id': id,
                     'fName': molecule_name,
                     'Mass': fraction_buried_volume,
                 })
@@ -91,10 +82,11 @@ def all_molecules():
         response_object['molecules'] = molecules
     return jsonify(response_object)
 
-#GET plot and DELETE molecule route handler
-@app.route('/<mol_id>', methods=['GET','DELETE'])
+
+# GET plot and DELETE molecule route handler
+@app.route('/<mol_id>', methods=['GET', 'DELETE'])
 def single_Mol(mol_id):
-    response_object = {'status':'succss'}
+    response_object = {'status': 'succss'}
     # delete the mol
     if request.method == 'DELETE':
         remove_mol(mol_id)
@@ -102,7 +94,7 @@ def single_Mol(mol_id):
 
     # Send the file in the response
     elif request.method == "GET":
-        plot_path = "backend/plots/plot_" + mol_id + ".png"
+        plot_path = PLOTS_DIRECTORY + "/plot_" + mol_id + ".png"
         if os.path.isfile(plot_path):
             plot_path = "plots\plot_" + mol_id + ".png"
             return send_file(plot_path, mimetype='image/png')
@@ -111,6 +103,7 @@ def single_Mol(mol_id):
             response_object['message'] = 'Failed to retrieve plot'
             return jsonify(response_object), 404
     return jsonify(response_object)
+
 
 def remove_mol(mol_id):
     for mol in molecules:
@@ -123,11 +116,11 @@ def remove_mol(mol_id):
     return False
 
 
-def getcenter(elements,non_metalic):
+def getcenter(elements, non_metalic):
     for metal_index in range(len(elements)):
         if elements[metal_index] in Metallic_atoms:
             return metal_index
-    if not non_metalic: 
+    if not non_metalic:
         raise Exception("   ")
     return non_metalic[0]
 
